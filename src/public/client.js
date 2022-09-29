5df53c80439d0f641286444270a00923199d1f31
kmwyatt <arc2437@naver.com>
2022-09-29 18:50:09 +0900

    enable chat list

--- src/public/client.js
+++ src/public/client.js
@@ -4,10 +4,13 @@
     const socket = new WebSocket(`ws://${window.location.host}/ws`);
     const formEl = document.getElementById('form');
     const inputEl = document.getElementById('input');
+    const chatsEl = document.getElementById('chats');
 
-    if (!formEl || !inputEl) {
+    if (!formEl || !inputEl || !chatsEl) {
         throw new Error('Init failed!');
     }
+
+    const chats = [];
 
     formEl.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -20,6 +23,14 @@
     });
 
     socket.addEventListener('message', (event) => {
-        alert(event.data);
+        chats.push(JSON.parse(event.data));
+
+        chatsEl.innerHTML = '';
+
+        chats.forEach(({ message, nickname }) => {
+            const div = document.createElement('div');
+            div.innerText = `${nickname}: ${message}`;
+            chatsEl.appendChild(div);
+        });
     });
 })();
